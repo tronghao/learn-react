5a04ee89376d721408d273a80aa1d6ef6b4f25f8
tronghao <tronghaomaico@gmail.com>
2020-10-28 19:26:19 +0700

    update 28_10_@0

--- material-dashboard-react-master/src/components/Input/create.js
+++ material-dashboard-react-master/src/components/Input/create.js
@@ -3,6 +3,7 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import {TextField, Button} from '@material-ui/core';
 import axios from 'axios';
 import { Field, reduxForm } from 'redux-form';
+import {connect} from "react-redux";
 
 const validate = values => {
     const errors = {}
@@ -19,13 +20,13 @@ const validate = values => {
   }
 
 const renderField = (props) => {
-	const { input, label, type, meta: { touched, error, warning } } = props;
-	const value = props.value;
+	const { input, label, valueInput, type, meta: { touched, error, warning } } = props;
+	input.value = valueInput;
 	return (
 	<div>
 	  <label className="control-label">{label}</label>
 	  <div>
-	    <input {...input} placeholder={label} type={type} className="form-control" />
+	    <input {...input} placeholder={label} type={type} className="form-control"/>
 	    {touched && ((error && <span className="text-danger">{error}</span>) || (warning && <span>{warning}</span>))}
 	  </div>
 	</div>
@@ -61,10 +62,10 @@ const Create = (props) => {
 			<form  onSubmit={ handleSubmit }>
 			  <div className="form-group">
 			  	{console.log(dataForm.name)}
-		        <Field name="name" component={renderField} label="Name" value={dataForm.name}/>
+		        <Field name="name" component={renderField} label="Name" valueInput={dataForm.name}/>
 		      </div>
 		      <div className="form-group">
-		        <Field name="age" component={renderField} label="Age" value={dataForm.age}/>
+		        <Field name="age" component={renderField} label="Age" valueInput={dataForm.age}/>
 		      </div>
 			  <br />
 			  <br />
@@ -81,10 +82,17 @@ const Create = (props) => {
 	
 }
 
-export default reduxForm({
+Create =  reduxForm({
   form: 'create', // a unique identifier for this form
   validate
 })(Create)
+
+export default connect(
+	state => ({
+    	initialValues: state.account.dataForm // pull initial values from account reducer
+  	}),
+  	{ load: loadAccount }   
+)(Create)
 
 
 
